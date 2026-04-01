d2ebae713e4c534c78782781f3fd44c9e43a4a0d
Thanh Hoang <hoang_thanh15@yahoo.com>
2026-03-31 23:45:42 -0700

    start react

---
+++ react-course/react-course-meta/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Nav from './components/Nav.jsx'
+import Intro1 from './components/Intro1.jsx'
+
+export function Header(){
+  return <h1>HELLO</h1>
+}
+
+function App() {
+return (
+  <div>
+    <Header/>
+    <Nav/>
+    <Intro1/>
+  </div>
+)}
+
+export default App
